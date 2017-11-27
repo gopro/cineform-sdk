180542a1f639fce78a274c2eea022c2e9b1c7c5c
Emeric Grange <egrange@gopro.com>
2017-11-27 19:02:18 +0100

    WIP: buildsystem with OS integration (v3)

---
+++ libcineformsdk.pc.cmake
@@ -0,0 +1,11 @@
+prefix=${CMAKE_INSTALL_PREFIX}
+exec_prefix=${EXEC_INSTALL_PREFIX}
+libdir=${LIB_INSTALL_DIR}
+includedir=${INCLUDE_INSTALL_DIR}
+
+Name: ${PROJECT_NAME}
+Description: CineForm SDK libraries
+URL: https://github.com/gopro/cineform-sdk
+Version: ${PROJECT_VERSION}
+Libs: -L${LIB_INSTALL_DIR} -lCFHDCodec -luuid -lm
+Cflags: -I${INCLUDE_INSTALL_DIR}
